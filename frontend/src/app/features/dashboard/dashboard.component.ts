199e20fdec1fab07db21b54914637f00a56ececf
SEBAS200309 <scardonaa22@academia.usbbog.edu.co>
2026-09-06 22:02:12 -0500

    CAMBIOS EN CSS

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -48,7 +48,7 @@ export class DashboardComponent implements OnInit {
     { icon: 'cast-outline', label: 'Leads', route: '/leads', tabla: 'leads' },
     { icon: 'person-outline', label: 'Personas', route: '/personas', tabla: 'personas' },
     { icon: 'briefcase-outline', label: 'Empresas', route: '/empresas', tabla: 'empresas' },
-    { icon: 'car-outline', label: 'Proveedores', route: '/proveedores', tabla: 'proveedores' },
+    { icon: 'car-outline', label: 'Proveedores Empresas', route: '/proveedores', tabla: 'proveedores' },
     { icon: 'car-outline', label: 'Proveedores Personas', route: '/proveedores-personas', tabla: 'proveedores-personas' },
     { icon: 'layers-outline', label: 'Servicios', route: '/servicios', tabla: 'servicios' },
     { icon: 'file-text-outline', label: 'Cotizaciones', route: '/cotizaciones', tabla: 'cotizaciones' },
