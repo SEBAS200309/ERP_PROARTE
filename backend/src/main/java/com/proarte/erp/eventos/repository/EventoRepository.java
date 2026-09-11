58c7d40feba414c187460c95968cabb4d1737277
SEBAS200309 <scardonaa22@academia.usbbog.edu.co>
2026-09-10 22:35:15 -0500

    camios evento repository

--- backend/src/main/java/com/proarte/erp/eventos/repository/EventoRepository.java
+++ backend/src/main/java/com/proarte/erp/eventos/repository/EventoRepository.java
@@ -20,6 +20,6 @@ public interface EventoRepository extends SoftDeleteRepository<Evento> {
     Page<Evento> searchByNombreAndEstadoId(@Param("search") String search, @Param("estadoId") UUID estadoId,
             Pageable pageable);
 
-    @Query(value = "SELECT COUNT(e.id) FROM evento e WHERE e.fecha_inicio >= DATE_TRUNC('week', CURRENT_DATE) AND e.fecha_inicio < DATE_TRUNC('week', CURRENT_DATE) + INTERVAL '7 days';", nativeQuery = true)
+    @Query(value = "SELECT public.cuenta_eventos();", nativeQuery = true)
     Integer eventosProximos();
 }
